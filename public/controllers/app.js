69eb35f0a9012a14686330908afb104ba01c678c
uodyne <uodyne@gmail.com>
2014-10-12 00:17:42 +0200

    Añadiendo la funcionalidad en el controlador

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -4,4 +4,36 @@ app.controller('mainCtrl', function mainCtrl($scope, $http) {
 
     $scope.formData = {};
 
+    $http.get('/models')
+        .success(function(data) {
+            $scope.models = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log("Error: " + data);
+        });
+
+    $scope.createModel = function() {
+        $http.post('/models', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.models = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log("Error: " + data);
+            });
+    };
+
+    $scope.deleteModel = function(id) {
+        $http.delete('/models/' + id)
+            .success(function(data) {
+                $scope.models = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log("Error: " + data);
+            });
+    };
+
 });
