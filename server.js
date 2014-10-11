76c71abe438a5f5477ccffc6469c95c76183fe62
uodyne <uodyne@gmail.com>
2014-10-11 20:56:01 +0200

    SERVER: Configuración servidor con express

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ var mongoose = require('mongoose');
 
 // Configuraciones
 var db = require('./config/db');
+
+
+// Servidor Express
+var app = express();
+// Configuración App
+app.configure(function() {
+    // Zona pública
+    app.use(express.static(__dirname + '/public'));
+
+    // Logs de las peticiones en consola
+    app.use(express.logger('dev'));
+
+    // Parseo de respuestas HTTP
+    app.use(express.json());
+    app.use(express.urlencoded());
+
+});
+
+// Iniciamos servidor en puerto 3000
+app.listen(3000);
