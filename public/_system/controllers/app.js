5049b67fe7dafd6d96dd7667f9929dbcaad62f19
uodyne <uodyne@gmail.com>
2014-10-12 17:06:51 +0200

    Route de la Home
    Y añadimos angular-animate para posteriormente hacer alguna animación
    de transición.

--- public/_system/controllers/app.js
+++ public/_system/controllers/app.js
@@ -1,5 +1,21 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'route-segment', 'view-segment']);
 
-app.controller('mainCtrl', function mainCtrl($scope) {
 
+app.config(function($routeSegmentProvider, $routeProvider) {
+
+    // Configuración
+    $routeSegmentProvider.options.autoLoadTemplates = true;
+
+    $routeSegmentProvider
+
+        .when('/home',          'home')
+        .segment('home', {
+            templateUrl: '_system/views/home.html',
+            controller: 'mainCtrl'})
+
+        $routeProvider.otherwise({redirectTo: '/home'});
+});
+
+app.controller('mainCtrl', function mainCtrl($scope, $routeSegment) {
+    $scope.$routeSegment = $routeSegment;
 });
